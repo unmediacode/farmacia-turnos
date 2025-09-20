6c417400b87c9e938c0bc6851781dd0f4e3864ab
Miguel_Taboada <unmedia@me.com>
2025-09-20 17:34:13 +0200

    Actualizar Astro y configurar Vercel

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,30 +1,7 @@
 import { defineConfig } from 'astro/config';
-import tailwind from '@astrojs/tailwind';
 import vercel from '@astrojs/vercel/serverless';
 
 export default defineConfig({
   output: 'server',
   adapter: vercel(),
-  integrations: [
-    tailwind({
-      applyBaseStyles: true
-    })
-  ],
-  compressHTML: true,
-  prefetch: {
-    defaultStrategy: 'viewport'
-  },
-  server: {
-    host: true,
-    port: 4321,
-    strictPort: true
-  },
-  vite: {
-    ssr: {
-      external: ['@libsql/client']
-    },
-    build: {
-      target: 'es2022'
-    }
-  }
 });
